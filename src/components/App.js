ece8bb53bd2f1b7b4df05233934968a39225f441
patricuismart <patricuismart@gmail.com>
2021-09-24 11:07:52 +0200

    Add filter clubs

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,16 @@ import initialData from '../data/contact.json';
 import { useState } from 'react';
 
 function App() {
+  //variables estado
+
   const [data, setData] = useState(initialData);
+  const [filter, setFilter] = useState('all');
   const [name, setName] = useState('');
   const [week, setWeek] = useState('');
   const [weekend, setWeekend] = useState('');
+
+  //Funciones manejadoras
+
   const handleName = (ev) => {
     setName(ev.currentTarget.value);
   };
@@ -27,28 +33,43 @@ function App() {
       openOnWeekdays: week,
       openOnWeekend: weekend,
     };
+    //Hago clon de lo que hay en data con spread operator y luego añador nuevo club
     setData([...data, newClub]);
   };
-  //variables estado
 
-  //Funciones manejadoras
+  const handleFilter = (ev) => {
+    setFilter(ev.target.value);
+  };
 
   //Para cada uno de los objetos de data va a generar ese html y lo acumula en una array, lo guardo en variable htmlClubList
-  const htmlClubList = data.map((oneClub, index) => (
-    <li key={index} className="club__item">
-      <p className="club__name">Nombre: {oneClub.name}</p>
-      <p className="club__week">
-        <label className="club__label">
-          Abierto entre semana: {oneClub.openOnWeekdays ? 'Sí' : 'No'}
-        </label>
-      </p>
-      <p className="club__weekend">
-        <label className="club__label">
-          Abierto fin de semana: {oneClub.openOnWeekdays ? 'Sí' : 'No'}
-        </label>
-      </p>
-    </li>
-  ));
+  //filtro en data y despues añado los arrays filtrados con map
+  const htmlClubList = data
+    .filter((openClub) => {
+      if (filter === 'week') {
+        return openClub.openOnWeekdays === true;
+      } else if (filter === 'weekend') {
+        return openClub.openOnWeekend === true;
+      } else {
+        return true;
+      }
+    })
+
+    //añade todos los clubs
+    .map((oneClub, index) => (
+      <li key={index} className="club__item">
+        <p className="club__name">Nombre: {oneClub.name}</p>
+        <p className="club__week">
+          <label className="club__label">
+            Abierto entre semana: {oneClub.openOnWeekdays ? 'Sí' : 'No'}
+          </label>
+        </p>
+        <p className="club__weekend">
+          <label className="club__label">
+            Abierto fin de semana: {oneClub.openOnWeekend ? 'Sí' : 'No'}
+          </label>
+        </p>
+      </li>
+    ));
 
   return (
     <div className="page">
@@ -56,8 +77,13 @@ function App() {
         <h1 className="header__title">Mis clubs</h1>
         <form>
           <label htmlFor="show">Mostrar: </label>
-          <select className="header__select" id="size" name="size">
-            <option value="All">Todos</option>
+          <select
+            className="header__select"
+            id="size"
+            name="size"
+            onChange={handleFilter}
+          >
+            <option value="all">Todos</option>
             <option value="week">los que abren entre semana</option>
             <option value="wekkend">los que abren el fin de semana</option>
           </select>
