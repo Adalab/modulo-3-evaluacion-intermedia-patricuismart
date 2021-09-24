02604a18521c17479304e2726606c2e67f2d0a10
patricuismart <patricuismart@gmail.com>
2021-09-24 08:57:16 +0200

    Give main structure and add data Json

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import '../styles/App.scss';
+//Import de datos de luista de contactos array de 4 objetos
+import initialData from '../data/contact.json';
+import { useState } from 'react';
+
+function App() {
+  //variables estado
+
+  //Funciones manejadoras
+
+  //Para cada uno de los objetos de data va a generar ese html y lo acumula en una array, lo guardo en variable htmlContactList
+  //filtro en data y despues añado los arrays filtrados
+
+  //filtro las búsqueda por nombre y por apellido
+
+  return <>Hola Mundo</>;
+}
+
+export default App;
